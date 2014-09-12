fa5104e58f9beccaa7a72a77b57d02a4f913ad8f
corvidism <a.frankova.contact@gmail.com>
2014-09-12 22:33:35 +0200

    Cleaning up leftovers.

--- fandomstats/__init__.py
+++ fandomstats/__init__.py
@@ -3,10 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 from fandomstats.api import api
 from fandomstats.home import home
-from fandomstats.bobble import bobble
 
 app = Flask(__name__)
 
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(home)
-app.register_blueprint(bobble,url_prefix='/bobble')
