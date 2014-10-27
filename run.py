740518b4cf5f7f13422b0dc22f13681e32c6a392
esgibter <esgibter@gmail.com>
2014-10-26 20:36:07 -0700

    first step of google app engine setup

--- run.py
+++
@@ -1,2 +0,0 @@
-from fandomstats import app
-app.run(debug = True)
---
+++ src/application/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+from flask import Flask
+import os
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.debug import DebuggedApplication
+
+app = Flask('application')
+
+if os.getenv('FLASK_CONF') == 'DEV':
+  #development settings n
+    app.config.from_object('application.settings.Development')
+  # Flask-DebugToolbar (only enabled when DEBUG=True)
+    toolbar = DebugToolbarExtension(app)
+    
+    # Google app engine mini profiler
+    # https://github.com/kamens/gae_mini_profiler
+    app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+
+    from gae_mini_profiler import profiler, templatetags 
+    @app.context_processor
+    def inject_profiler():
+        return dict(profiler_includes=templatetags.profiler_includes())
+    app.wsgi_app = profiler.ProfilerWSGIMiddleware(app.wsgi_app)
+
+elif os.getenv('FLASK_CONF') == 'TEST':
+    app.config.from_object('application.settings.Testing')
+
+else:
+    app.config.from_object('application.settings.Production')
+
+# Enable jinja2 loop controls extension
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
+from application.api import api
+from application.home import home
+
+app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(home)
