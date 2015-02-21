7f5fe660e5ef3f3d7047c23ba7f9cea2f346a1db
esgibter <esgibter@gmail.com>
2015-02-21 13:24:38 -0800

    adding better error handling

--- src/application/api/views.py
+++ src/application/api/views.py
@@ -46,6 +46,7 @@ class Stats(Resource):
     s = AO3data()
     url = AO3url()
     url.setFilters(parser.parse_args())
+    print url.getUrl()
     return s.getTopInfo(url.getUrl())
 
 class TagStats(Resource):
