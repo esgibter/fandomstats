f20e9f00b388906026e9b5457166d898da8b35d9
annathecrow <annathecrow@corvidism.com>
2015-12-27 21:29:00 +0100

    new pages, redone navbar so it highlights current page, hidden the fandom research directory for now, layout changes

--- src/application/home/views.py
+++ src/application/home/views.py
@@ -5,3 +5,24 @@ from application.home import home
 @home.route("/")
 def index():
   return render_template('homepage.html')
+  
+# AO3 Bookmark Viewer
+@home.route("/ao3-bookmark-viewer")
+def bookmark_viewer():
+  return render_template('bookmark-viewer.html')
+  
+# AO3 Tag Stats
+@home.route("/ao3-tag-stats")
+def tag_stats():
+  return render_template('tag-stats.html')
+
+# Fandom Research Directory
+@home.route("/fandom-research-directory")
+def linkspam():
+  return render_template('linkspam.html')
+
+# About
+@home.route("/about")
+def about():
+  return render_template('about.html')
+
