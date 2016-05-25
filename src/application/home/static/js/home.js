765010167fc6a5e6e7a6524d125d45a7c75cdf72
annathecrow <annathecrow@corvidism.com>
2016-05-25 14:00:56 +0100

    about page

--- src/application/home/static/js/home.js
+++ src/application/home/static/js/home.js
@@ -90,3 +90,13 @@ $("#search-btn").click(function(e){
 	$("#searchform").submit();
 	return false;
 });
+
+var addresses = $("span.c-addr");
+
+//function that formats email addresses, so they're not in full form in the static text and are (reasonably) protected from spam scrapers.
+$.each(addresses, function(elem) {
+	var addressPlaceholderElem = $(this);
+	var address = addressPlaceholderElem.text().replace("-a-","@").replace("-d-",".");
+	var addressLinkElem = $('<a href="mailto:'+address+'">'+address+'</a>');
+	addressPlaceholderElem.replaceWith(addressLinkElem);
+});
