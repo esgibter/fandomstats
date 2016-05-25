40915e6021a0d89c2cd67a5b47aaaeab7597ca91
annathecrow <annathecrow@corvidism.com>
2016-05-25 17:06:02 +0100

    added text for ao3-tag-stats; homepage now returns API results in csv; fixed error notification for non-existent tags

--- src/application/home/static/js/home.js
+++ src/application/home/static/js/home.js
@@ -1,5 +1,13 @@
 $(".variant").hide();
 $(".variant.selected").show();
+
+function commaConcat(obj) {
+	var string = "";
+	$.each(obj, function(index, value){
+		string += index + ", " + value + "\n";
+	});	
+	return string;
+}
   
 //more link in description 
 moreDesc = $(".more-text");
@@ -20,18 +28,19 @@ $("a.more").show().click(function(){
 	return false;
 });
 
-var jsonField = $("#json-field");
-jsonField.val("");
+var resultField = $("#result-field");
+resultField.val("");
 
-$("#json-field").click(function(){
+$("#result-field").click(function(){
 	this.focus();
 	this.select();
 });
 
-$("#searchform").submit(function(e){
+$("#searchform, #searchform-homepage").submit(function(e){
 	e.preventDefault();
 	searchform = $(this);
-	var button = $("#search-btn");
+	searchform.find(".form-info").remove();
+	var button = searchform.find("#search-btn");
 	var buttonContent = button.html();
 	button.html($("#spinner").clone().show());
 	var tagAPI = {};
@@ -45,40 +54,43 @@ $("#searchform").submit(function(e){
 		},
 		success: function(result, status, object){
 			$(".api-results").show('fast');
-			console.log(object.status);
-			$("#json-field").val(object.responseText); 
+			console.log(result);
+			if ("searchform-homepage" == searchform.attr("id")) {
+				var csv = '';
+				csv += "total number of works in tag, "+result.numworks+"\n";
+				$.each(result.stats, function(index,data) {
+					csv += "\n";
+					csv += index + ", number of works\n";
+					csv += commaConcat(result.stats[index]);
+				});			
+				
+				$("#result-field").val(csv);
+				
+			} else {
+				$("#result-field").val(object.responseText);
+			}			 
 		},
 		error: function(object,exception) {
+			$("#result-field").val("");
 			if (object.status === 0) {
-				//connection error
-			} else if (object.status == 404) {
+				//connection error				
+			} else if (object.status == 404 || object.status == 400) {
 				//not found
 				searchform.append('<p class="form-info">This tag wasn\'t found on AO3.</p>');				
 			} else if (object.status == 501) {
 				//redirection
 				searchform.append('<p class="form-info">Looks like this is a non-canonical tag! Unfortunately, our API can\'t follow redirects yet. Try <a href="http://archiveofourown.org/tags/search?utf8=%E2%9C%93&query[name]='+tag+'&query[type]=">looking up the canonical tag on AO3</a>.</p>');								
-			} else if (object.status == 500){
+			} else if (object.status == 500 || exception === 'parsererror' || exception === 'timeout' || exception === 'abort'){
 				//internal error
 				button.html($("#button-error").clone().show());
 				searchform.append('<p class="form-info">An error occured.<a href="'+window.location.href+'">Refresh page to try again.</a></p>');
-			} else if (exception === 'parsererror') {
-				button.html($("#button-error").clone().show());
-				searchform.append('<p class="form-info">An error occured.<a href="'+window.location.href+'">Refresh page to try again.</a></p>');
-				//request failed
-			} else if (exception === 'timeout') {
-				button.html($("#button-error").clone().show());
-				searchform.append('<p class="form-info">An error occured.<a href="'+window.location.href+'">Refresh page to try again.</a></p>');
-				//timeout error
-			} else if (exception === 'abort') {
-				button.html($("#button-error").clone().show());
-				searchform.append('<p class="form-info">An error occured.<a href="'+window.location.href+'">Refresh page to try again.</a></p>');
-				//aborted
 			} else {
 				//something else
 				//echo object.responseText
 			}
 		},
 		complete:function(object,status) {
+			console.log(object);
 			button.html(buttonContent); //loader back to text
 		}
 	});
@@ -87,7 +99,7 @@ $("#searchform").submit(function(e){
 
 $("#search-btn").click(function(e){
 	e.preventDefault();
-	$("#searchform").submit();
+	$(this).closest(".searchform").submit();
 	return false;
 });
 
