bf3240298251a6522e15d04ed365f56753ec04c8
annathecrow <annathecrow@corvidism.com>
2016-09-19 00:37:21 +0200

    one graph done

--- src/application/home/static/js/homepage.js
+++ src/application/home/static/js/homepage.js
@@ -1,10 +1,18 @@
-function commaConcat(obj) {
-	var string = "";
-	$.each(obj, function(index, value){
-		string += index + ", " + value + "\n";
-	});	
-	return string;
-}
+
+//######## RUN ##########//
+
+$(window).resize(debounce(resize,250));
+
+
+
+//######### HOOKS #########//
+
+
+$(document).ready(function(){
+	//$("#search-string").val("fluff");
+	//$(".searchform").submit();	
+});
+
 
 $(".searchform").submit(function(e){
 	e.preventDefault();
@@ -27,11 +35,15 @@ $(".searchform").submit(function(e){
 			console.log(result);
 			//number of works (big number)
 			//ratings (piechart)
-			plotCategories(result.stats.category,result.numworks);
 			//warnings (percent bar)
-			
 			//the rest (bar charts)
-			renderCsv(result);			 
+			var graphs = $("#main-graphs");
+			graphs.css("visibility","visible");
+			plotCategories(result.stats.category,result.numworks);
+			
+			$('<div id="api-export" class="large-12 column"><label>results as CSV:</label><textarea placeholder="" id="result-field" readonly="readonly"></textarea></div>').appendTo(graphs);
+			renderCsv(result);
+										 
 		},
 		error: function(object,exception) {
 			$("#result-field").val("");
@@ -53,13 +65,14 @@ $(".searchform").submit(function(e){
 			}
 		},
 		complete:function(object,status) {
-			console.log(object);
 			button.html(buttonContent); //loader back to text
 		}
 	});
 	
 });
 
+
+//################ FUNCTIONS #################//
 
 function renderCsv(result) {
 	var csv = '';
@@ -73,19 +86,9 @@ function renderCsv(result) {
 	$("#result-field").val(csv);	
 }
 
-// ----------- TESTING -------------------
-
-$(document).ready(function(){
-	//$("#search-string").val("fluff");
-	//$(".searchform").submit();
-	var cats = { "Gen": 33453, "Other": 5355, "Multi": 13294, "F/F": 23684, "M/M": 208583, "F/M": 82023 };
-	var sum = 324267;
-	$(".api-results").show();
-	plotCategories(cats,sum);	
-});
-
 
 //----------------------- D3 JS HELL ---------------------
+
 
 var margin = {
 		top:50,
@@ -94,14 +97,31 @@ var margin = {
 		bottom:50,
 };
 
-//TODO: make dynamic
-var width = 600 - margin.left - margin.right;
-var height = 300 - margin.top - margin.bottom;
+
+function resize() {
+	//https://www.safaribooksonline.com/blog/2014/02/17/building-responsible-visualizations-d3-js/
+}
+
+
 
 //TODO: function resize()
 
+function numWorks() {
+	//TODO: big number of works (potentially: get all works on AO3 from the logged-out screen -- relevant github issue https://github.com/esgibter/fandomstats/issues/40)
+}
 
-function plotCategories(category,sum) {
+function plotCategories(category, sum) {
+	
+	//TODO: just redraw with new values if it already exists (e.g. a new API call)
+	
+	var fullWidth = $("#main-graphs").width();
+	console.log("fullWidth: "+fullWidth);
+	var graphWidth = $("#graph-category").width();
+	console.log("graphWidth: "+graphWidth);
+	var graphRatio = 4/9;
+	//TODO: make dynamic
+	var width = graphWidth - margin.left - margin.right;
+	var height = graphWidth*graphRatio - margin.top - margin.bottom;
 	
 	var dataset = [];
 	
@@ -127,6 +147,7 @@ function plotCategories(category,sum) {
 	
 	var svg = d3.select("#graph-category")
 				.append("svg")
+				.attr("class","has-graph")
 				.attr("width",width + margin.left + margin.right)
 				.attr("height",height + margin.top + margin.bottom)
 				.append("g")
@@ -143,7 +164,7 @@ function plotCategories(category,sum) {
 	
 	var yScale = d3.scaleBand()
 				.domain(dataset.map(function(d) {return d.y;}))
-				.rangeRound([height,0])
+				.rangeRound([0,height]) //I'm not "flipping" the axis here, because I want the bars to start at top
 				.padding(0.05);
 				
 				
@@ -199,19 +220,27 @@ function plotCategories(category,sum) {
 				.data(dataset)
 				.enter()
 				.append("rect")
-				.attr("x", function(d) {
-					return xScale(0);
-				})
-				.attr("y", function(d) {
-					return yScale(d.y);
-				})
-				.attr("height", yScale.bandwidth())
-				.attr("width", function(d) {
-					//console.log("=========");
-					//console.log(d);
-					return xScale(d.x);
-				})
-				.attr("fill","#cc0000");
+					.attr("x", function(d) {
+						return xScale(0);
+					})
+					.attr("y", function(d) {
+						return yScale(d.y);
+					})
+					.attr("height", yScale.bandwidth())
+					.attr("width", function(d) {
+						return 0;						
+					})
+					.attr("fill","#cc0000")
+				.transition()
+					.delay(function(d, i) { return i * 100; })
+					.duration(1000)
+					.attr("width", function(d){
+						//console.log("=========");
+						//console.log(d);
+						return xScale(d.x);
+					})
+				;
+				
 			
 	var percents = svg.append("g")
 				.attr("class","percents");
@@ -229,9 +258,7 @@ function plotCategories(category,sum) {
 				.attr("dy","0.35em")
 				.attr("height",yScale.bandwidth())
 				.text(function(d) {
-					var perc = d.percent * 100;
-					
-					return Math.round(perc) + "%";
+					return d3.format(">.0%")(d.percent);					
 				})
 				.attr("font-size", "0.8em")
 				.attr("fill","#999");
@@ -257,7 +284,7 @@ function plotCategories(category,sum) {
 				.attr("dy","0.35em")
 				.attr("height",yScale.bandwidth())
 				.text(function(d) {
-					return d.x;
+					return d3.format(",")(d.x);
 				})
 				.attr("fill", function(d) {
 					var left = xScale(d.x);
@@ -276,4 +303,38 @@ function plotCategories(category,sum) {
 					}
 				});	
 }
+
+
+
+
+//--------------- UTILITIES --------------------//
+
+function commaConcat(obj) {
+	var string = "";
+	$.each(obj, function(index, value){
+		string += index + ", " + value + "\n";
+	});	
+	return string;
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+//thx to David Walsh (https://davidwalsh.name/javascript-debounce-function), for not making me write my own debounce fn!
+
 
