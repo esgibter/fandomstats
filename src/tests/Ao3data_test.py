10ef06ff7b1b6cc111a159e433f0db79b17f84a9
annathecrow <annathecrow@corvidism.com>
2017-07-08 17:56:02 +0200

    added some tests for the Ao3data method, rewrote the method so it can be tested at all...

---
+++ src/tests/Ao3data_test.py
@@ -0,0 +1,44 @@
+import os,sys
+sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
+import application as fandomstats
+import unittest
+import requests
+import tempfile
+from werkzeug import datastructures 
+from application.api.models import AO3data
+
+class Ao3dataTestCase(unittest.TestCase):
+    
+    def setUp(self):
+      fandomstats.app.config['TESTING'] = True
+      fandomstats.app.config['DEBUG'] = True
+      self.app = fandomstats.app.test_client()
+      self.maxDiff = None
+        
+    def test_fetchHTML_very_bad_url(self):
+        url = "boingboing"
+        a = AO3data(url)
+        
+        with self.assertRaises(ValueError) as context:
+            data = a.fetchHTML(url)
+        
+        self.assertEquals(context.exception.args[0],400)
+    
+    def test_fetchHTML_nonexistent_tag(self):
+        url = "http://archiveofourown.org/works?tag_id=boingboing"
+        a = AO3data(url)
+        
+        with self.assertRaises(ValueError) as context:
+            data = a.fetchHTML(url)
+        
+        self.assertEquals(context.exception.args[0],404)
+    
+    @unittest.skip("not yet")    
+    def test_fetchHTML_basic(self):
+        url = "http://archiveofourown.org/works?tag_id=Harry+Potter"
+        expected = []
+        a = AO3data(url)
+        self.assertEqual(a.fetchHTML(url),expected)
+
+if __name__ == '__main__':
+    unittest.main()
